bafc623563bfadf76467bac0f50d91b9b2b44a92
kollnerdani <dankollner@gmail.com>
2023-03-23 19:31:16 +0100

    filter search opt

--- src/App.js
+++ src/App.js
@@ -30,13 +30,15 @@ class App extends Component {
   }
 
   render (){
-      const filteredMonsters = this.state.monsters.filter((monster) =>{
-          return monster.name.toLowerCase().includes(this.state.searchField)
+      const { monsters, searchField } = this.state
+      const { search } = this
+      const filteredMonsters = monsters.filter((monster) =>{
+          return monster.name.toLowerCase().includes(searchField)
       })
 
       return (
         <div className="App">
-            <input className='search-box' type="text" placeholder='search' onChange={this.search}/>
+            <input className='search-box' type="text" placeholder='search' onChange={search}/>
             {filteredMonsters.map((monster) =>{
                return (
                    <div key={monster.id}>
