56e498aa88b283b2fdf7e58b0edd9baafb16ee85
kollnerdani <dankollner@gmail.com>
2023-03-24 15:01:19 +0100

    monster desruct

--- src/components/monster_list/index.jsx
+++ src/components/monster_list/index.jsx
@@ -7,13 +7,16 @@ class MonsterList extends Component {
         const { monsters } = this.props
         return(
             <div>
-                { monsters.map( monster => (
-                    <div className='card-container' key={monster.id}>
-                        <img src={`https://robohash.org/${monster.id}?set=set2&size=180x180`} alt={`monster ${monster.name}`}/>
-                        <h2>{monster.name}</h2>
-                        <p>{monster.email}</p>
-                    </div>
-                ))}
+                { monsters.map( (monster) => {
+                    const { name, id, email } = monster
+                    return(
+                        <div className='card-container' key={monster.id}>
+                            <img src={`https://robohash.org/${monster.id}?set=set2&size=180x180`} alt={`monster ${monster.name}`}/>
+                            <h2>{monster.name}</h2>
+                            <p>{monster.email}</p>
+                        </div>
+                    )
+                })}
             </div>
         )
     }
