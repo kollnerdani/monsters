1fd17b1fc485ae90b32067d38a205a9746567032
kollnerdani <dankollner@gmail.com>
2023-03-24 17:43:54 +0100

    search bar component

--- src/components/search_bar/index.jsx
+++ src/components/search_bar/index.jsx
@@ -1,17 +1,12 @@
-import { Component } from 'react';
 import './search_bar.css'
-class SearchBar extends Component {
+const SearchBar = ({ className, placeholder, onChangeHandler}) => (
+        <input
+            className={`search-box ${className}`}
+            type="text"
+            placeholder={placeholder}
+            onChange={onChangeHandler}
+        />
+    )
 
-    render () {
-        const search= this.props
-        return(
-            <input
-                className={`search-box ${this.props.className}`}
-                type="text"
-                placeholder={this.props.placeholder}
-                onChange={this.props.onChangeHandler}/>
-        )
-    }
-}
 
 export default SearchBar;
